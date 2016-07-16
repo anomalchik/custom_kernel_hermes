34af6a3ff454942249259cca21b40b58c62e8c57
Anomalchik <vidslava@yandex.ru>
2016-07-16 19:50:09 +0300

    kdsensor: remove lenovo features

--- kernel-3.10/drivers/misc/mediatek/imgsensor/src/mt6795/kd_sensorlist.c
+++ kernel-3.10/drivers/misc/mediatek/imgsensor/src/mt6795/kd_sensorlist.c
@@ -1767,8 +1767,8 @@ inline static int  adopt_CAMERA_HW_FeatureControl(void *pBuf)
 	    g_NewSensorExpGain.uISPGainDelayFrame = 0xFF;
 	    spin_unlock(&kdsensor_drv_lock);
     /*lenovo-sw sunliang modify for long_shutter 2015_4_25 begin*/
-        case SENSOR_FEATURE_LOCK_AE:
-        case SENSOR_FEATURE_UNLOCK_AE:
+     //   case SENSOR_FEATURE_LOCK_AE:
+      //  case SENSOR_FEATURE_UNLOCK_AE:
     /*lenovo-sw sunliang modify for long_shutter 2015_4_25 end*/
 	case SENSOR_FEATURE_SET_ISP_MASTER_CLOCK_FREQ:
 	case SENSOR_FEATURE_SET_REGISTER:
